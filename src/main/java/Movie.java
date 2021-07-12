06cb792f67c03fd8f9289664b191a46ec5a944d4
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-12 16:36:32 +0900

    feat: 예매 도메인 개발

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,2 +1,24 @@
+import java.math.BigDecimal;
+import java.time.Duration;
+
 public class Movie {
+    private String title;
+    private Duration runningtime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningtime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningtime = runningtime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return this.fee;
+    }
+
+    public Money calculateMovieFee(Screening screening){
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
 }
