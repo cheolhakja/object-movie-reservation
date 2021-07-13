cf5b96156bc71a4be925d774fe488a49f30490bd
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-13 13:30:20 +0900

    feat: 인터페이스를 만들고 이를 구현한 NoneDiscountPolicy 추가

--- src/main/java/object/discountpolicy/DiscountPolicy.java
+++ src/main/java/object/discountpolicy/DiscountPolicy.java
@@ -2,27 +2,7 @@ package object.discountpolicy;
 
 import object.Money;
 import object.Screening;
-import object.discountcondition.DiscountCondition;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public abstract class DiscountPolicy {
-    private List<DiscountCondition> conditions = new ArrayList<DiscountCondition>();
-
-    public DiscountPolicy(DiscountCondition ... conditions) {
-        this.conditions = Arrays.asList(conditions);
-    }
-
-    public Money calculateDiscountAmount(Screening screening){
-        for(DiscountCondition each : conditions) {
-            if(each.isSatisfiedBy(screening)) {
-                return getDiscountAmount(screening);
-            }
-        }
-        return Money.ZERO;
-    }
-
-    abstract protected Money getDiscountAmount(Screening screening);
+public interface DiscountPolicy {
+    public Money calculateDiscountAmount(Screening screening);
 }
